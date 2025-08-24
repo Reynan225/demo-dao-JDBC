3ca5620f66ab924a9c6b5d81f869fa78b45e979c
Reynan <reynandan11@gmail.com>
2025-08-24 20:58:12 -0300

    findById implementation

--- src/Program/application.java
+++ src/Program/application.java
@@ -1,10 +1,18 @@
 package Program;
 
+import model.dao.SellerDao;
+import model.dao.impl.DaoFactory;
+import model.entities.Seller;
+
 public class application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SellerDao sl = new DaoFactory().createSellerDao();
+		
+		Seller seller = sl.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/impl/DaoFactory.java
+++ src/model/dao/impl/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import db.DB;
 import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 
@@ -8,10 +9,10 @@ public class DaoFactory {
 	/*Essa classe será responsável por inicializar as
 	 * nossas classes DAOJDBC, isso é uma boa prática
 	 * para evitar que a implementação da interface
-	 * seja exposta e fácil de manutenção */
+	 * seja exposta e fácil de manuntenção */
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 		public static DepartmentDao createDepartmentDao() {
 			return new DepartmentDaoJDBC();
